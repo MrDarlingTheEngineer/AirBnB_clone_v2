94c4e8109a89eea6499b78d13e6671a04c08f22f
Mr_Darling <117775164+MrDarlingTheEngineer@users.noreply.github.com>
2024-07-29 04:03:36 +0000

    webflask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,93 +1,64 @@
-
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
-import json
+"""module of 'FileStorage' class"""
 
-from models.amenity import Amenity
+import os.path
+import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+
+class_list = {"BaseModel": BaseModel,
+              "User": User,
+              "State": State,
+              "City": City,
+              "Amenity": Amenity,
+              "Place": Place,
+              "Review": Review
+              }
+white_list = []
+for key in class_list:
+    white_list.append(key)
 
 
-class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+class FileStorage():
+    """Representation of a FileStorage"""
 
     __file_path = "file.json"
     __objects = {}
 
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage.
+    def __init__(self):
+        """class constructor"""
+        pass
 
-        Args:
-            cls (class, optional): If specified, filters the result to include
-                only objects of the specified class.
-
-        Returns:
-            dict: A dictionary containing objects in storage.
-        """
-        if cls:
-            if isinstance(cls, str):
-                cls = globals().get(cls)
-            if cls and issubclass(cls, BaseModel):
-                cls_dict = {
-                    k: v
-                    for k, v in self.__objects.items()
-                    if isinstance(v, cls)
-                }
-                return cls_dict
-        return FileStorage.__objects
+    def all(self):
+        """returns the dictionary '__objects'"""
+        return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
+        """sets in '__objects' the 'obj' with key '<obj class name>.id'"""
+        key = obj.__class__.__name__+"."+obj.id
+        self.__objects.update({key: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, "w") as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        """serializes '__objects' to the JSON file (__file_path)"""
+        dict = {}
+        for key in self.__objects:
+            dict[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(dict, f)
 
     def reload(self):
-        """Loads storage dictionary from file."""
-        classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "Place": Place,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Review": Review,
-        }
+        """deserializes the JSON file to '__objects'
+           (only if the JSON file (__file_path) exists, otherwise do nothing.
+           If the file doesn’t exist, no exception should be raised)"""
         try:
-            temp = {}
-            with open(FileStorage.__file_path, "r") as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val["__class__"]](**val)
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                dict = json.load(f)
+            for i in dict:
+                self.__objects[i] = class_list[dict[i]["__class__"]](**dict[i])
+        except:
             pass
-        except json.decoder.JSONDecodeError:
-            pass
-
-    def delete(self, obj=None):
-        """
-        Delete obj from __objects if it’s inside - if obj is equal to None,
-          the method should not do anything
-        """
-        if obj is None:
-            return
-        obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
-
-        try:
-            del FileStorage.__objects[obj_to_del]
-        except AttributeError:
-            pass
-        except KeyboardInterrupt:
-            pass
-
--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /hbnb: HBnB home page.
@@ -29,4 +28,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port='5000')
