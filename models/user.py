794841c9ebfb31b396766cd3683f345233cd35f5
Mr_Darling <117775164+MrDarlingTheEngineer@users.noreply.github.com>
2024-07-29 03:41:21 +0000

    webflask

--- models/user.py
+++ models/user.py
@@ -1,47 +1,29 @@
+
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from hashlib import md5
+from models.place import Place
+from models.review import Review
+
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        __table_args__ = (
-            {'mysql_default_charset': 'latin1'})
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place",
-				cascade="all,delete", 
-				backref=backref("user"),
-				passive_deletes=True,
-				single_parent=True)
-        reviews = relationship("Review",
-				backref=backref("user")
-				cascade="all,delete",
-				passive_deletes=True,
-				single_parent=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-	"""if "password" in kwargs:
-            kwargs["password"] = \
-                md5(kwargs["password"].encode('utf-8')).digest()"""
-        super().__init__(*args, **kwargs)
-    def __setattr__(self, att_name, value):
-        """setattr for encrypt on new password"""
-        if att_name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(att_name, value)
--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
+
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /hbnb: HBnB home page.
@@ -28,4 +29,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port='5000')
+    app.run(host="0.0.0.0")
